cf6d1a9794fd2d2526fe4d62b242043dbd1bc9c2
kailian <244970749@qq.com>
2014-03-04 09:12:20 +0800

    Update getip.php

--- getip.php
+++ getip.php
@@ -4,7 +4,7 @@
  *
  * @return unknown
  */
-echo("hello world");
+echo("hello world test");
 function GetIP() {
     if (! empty( $_SERVER ["HTTP_CLIENT_IP"] ))
         $cip = $_SERVER ["HTTP_CLIENT_IP"];
